14c32b4c99b6356b0531a24383653e93f1c731d8
Oren Teich <oren@teich.net>
2025-06-22 18:17:18 -0700

    force dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,8 @@ export const metadata: Metadata = {
   description: "Business Dashboard",
 };
 
+export const dynamic = 'force-dynamic';
+
 export default function RootLayout({
   children,
 }: Readonly<{
